02786977ebaf3bb9b644468040bd006e0aa52dd0
cavaly <37452401+cavaly@users.noreply.github.com>
2018-03-21 21:24:47 +0700

    zxc
    nothing much

---
+++ RescueMyDoggo/Resources/Test M1/island.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="island" tilewidth="32" tileheight="32" tilecount="1134" columns="42">
+ <image source="shutter_island_by_ryky-da1lvdb.png" width="1361" height="870"/>
+</tileset>
---
+++ RescueMyDoggo/Resources/the_ghost_by_ryky-d9ik1z6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="the_ghost_by_ryky-d9ik1z6" tilewidth="32" tileheight="32" tilecount="798" columns="42">
+ <image source="the_ghost_by_ryky-d9ik1z6.jpg" width="1371" height="623"/>
+</tileset>
