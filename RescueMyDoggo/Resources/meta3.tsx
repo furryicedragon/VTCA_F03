f9db6a0319b665c155cb0526b8d4d9171517877e
cavaly <37452401+cavaly@users.noreply.github.com>
2018-06-17 21:51:10 +0700

    fixed map portal location

--- RescueMyDoggo/Resources/meta3.tsx
+++ RescueMyDoggo/Resources/meta3.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="meta3" tilewidth="16" tileheight="16" tilecount="2" columns="2">
  <image source="meta3.png" width="32" height="16"/>
+ <tile id="1">
+  <properties>
+   <property name="Collidable" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
